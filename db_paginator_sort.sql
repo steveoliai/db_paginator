756517fdc8c838c7980531076150286d20eea417
steveoliai <72261170+steveoliai@users.noreply.github.com>
2021-12-26 16:07:13 -0500

    removed schema qualifier

--- db_paginator_sort.sql
+++ db_paginator_sort.sql
@@ -12,7 +12,7 @@
 --------------------------------------------------------------------------------------------------------------------------------------*/
 --table for logging/resuming report
 
-create table acct15m.report_settings (
+create table report_settings (
     reportid number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
     reportname varchar2(128),
     dayinterval number,
@@ -21,7 +21,7 @@ create table acct15m.report_settings (
     primary key (reportid)
 );
 
-create table acct15m.report_instance (
+create table report_instance (
     reportinstanceid number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),,
     reportid number, 
     sessionid varchar2(128),
@@ -45,7 +45,7 @@ create table acct15m.report_instance (
 
 --table for logging/resuming report
 
-create table acct15m.report_scroll (
+create table report_scroll (
     reportscrollid number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
     reportinstanceid number,
     sessionid varchar2(128),
@@ -62,12 +62,12 @@ create table acct15m.report_scroll (
     primary key (reportscrollid)
 );
 
---create index report_scroll_check on acct15m.report_scroll(upper(sessionid), upper(reportname));
+--create index report_scroll_check on report_scroll(upper(sessionid), upper(reportname));
 
 
 
 
-create or replace procedure  acct15m.REPORT_PAGINATOR(t_reportname varchar2, t_collist VARCHAR2, t_reportconditions varchar2, t_pkcolname varchar2, t_datecolname varchar2, t_sessionid IN VARCHAR2, t_numrows IN NUMBER, t_dtfrom IN DATE, t_dtto IN DATE, t_pageno_in IN NUMBER, t_sort IN VARCHAR2, t_hasmore OUT CHAR, t_rec_output OUT SYS_REFCURSOR)
+create or replace procedure  REPORT_PAGINATOR(t_reportname varchar2, t_collist VARCHAR2, t_reportconditions varchar2, t_pkcolname varchar2, t_datecolname varchar2, t_sessionid IN VARCHAR2, t_numrows IN NUMBER, t_dtfrom IN DATE, t_dtto IN DATE, t_pageno_in IN NUMBER, t_sort IN VARCHAR2, t_hasmore OUT CHAR, t_rec_output OUT SYS_REFCURSOR)
 AS
 t_check NUMBER;
 t_checkrpt NUMBER;
@@ -94,13 +94,13 @@ t_reportinstanceid NUMBER;
 BEGIN
 
     if t_pageno_in is null then -- not lookng for a prebuilt page
-        select count(*) into t_checkforce from acct15m.report_settings where upper(reportname) = upper(t_reportname) and forcesetting = 'Y';
+        select count(*) into t_checkforce from report_settings where upper(reportname) = upper(t_reportname) and forcesetting = 'Y';
         if t_checkforce = 1 then 
-            select dayinterval into t_dayinterval from acct15m.report_settings where upper(reportname) = upper(t_reportname) and forcesetting = 'Y' order by modifiedon desc fetch first 1 rows only;
+            select dayinterval into t_dayinterval from report_settings where upper(reportname) = upper(t_reportname) and forcesetting = 'Y' order by modifiedon desc fetch first 1 rows only;
             --could add logic here to select a recent interval if one exists to avoid extra work below    
         end if;
         --need to find out if this report has been run in the last hour
-        select nvl(max(reportinstanceid), 0) into t_reportinstanceid from acct15m.report_instance where upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and upper(columnlist) = upper(t_collist) and upper(reportconditions) = upper(t_reportconditions) and upper(sort) = upper(t_sort) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
+        select nvl(max(reportinstanceid), 0) into t_reportinstanceid from report_instance where upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and upper(columnlist) = upper(t_collist) and upper(reportconditions) = upper(t_reportconditions) and upper(sort) = upper(t_sort) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
         if t_reportinstanceid = 0 then --this is the first run
             t_newrunfrom := t_dtfrom;
             t_newrunto := t_dtto;
@@ -147,11 +147,11 @@ BEGIN
             -- get the last page generated for the report
             -- get values from the last run to pass to the ref cursor
             if upper(t_sort) = 'ASC' then
-                select pageno, pagedateto, pkid, dayinterval, numrecsinrpt into t_pageno, t_newrunfrom, t_pkid, t_dayinterval, t_numrecsinrpt from acct15m.report_instance where reportinstanceid = t_reportinstanceid order by createdon desc fetch first 1 rows only;
+                select pageno, pagedateto, pkid, dayinterval, numrecsinrpt into t_pageno, t_newrunfrom, t_pkid, t_dayinterval, t_numrecsinrpt from report_instance where reportinstanceid = t_reportinstanceid order by createdon desc fetch first 1 rows only;
                 t_pageno := t_pageno + 1; --adding one here for insert below            
                 t_newrunto := t_newrunfrom + t_dayinterval;
             else
-                select pageno, pagedatefrom, pkid, dayinterval, numrecsinrpt into t_pageno, t_newrunto, t_pkid, t_dayinterval, t_numrecsinrpt from acct15m.report_instance where reportinstanceid = t_reportinstanceid order by createdon desc fetch first 1 rows only;
+                select pageno, pagedatefrom, pkid, dayinterval, numrecsinrpt into t_pageno, t_newrunto, t_pkid, t_dayinterval, t_numrecsinrpt from report_instance where reportinstanceid = t_reportinstanceid order by createdon desc fetch first 1 rows only;
                 t_pageno := t_pageno + 1; --adding one here for insert below
                 t_newrunfrom := t_newrunto - t_dayinterval; 
             end if;     
@@ -213,26 +213,26 @@ BEGIN
 
 
         --check if we have a report entry w/o forced params
-        select nvl(max(reportid), 0) into t_checkrpt from acct15m.report_settings where upper(reportname) = upper(t_reportname);
+        select nvl(max(reportid), 0) into t_checkrpt from report_settings where upper(reportname) = upper(t_reportname);
         if t_checkrpt = 0 then
-            insert into acct15m.report_settings (reportname, dayinterval) values (t_reportname, t_dayinterval);
+            insert into report_settings (reportname, dayinterval) values (t_reportname, t_dayinterval);
         end if;
 
         --insert our settings/info
         if t_pageno = 1 then --insert the instance record
-            insert into acct15m.report_instance(sessionid, reportname, reportconditions, columnlist, pageno, numrows, numrecsinrpt, datefrom, dateto, pagedatefrom, pagedateto, pkid, dayinterval, sort)
+            insert into report_instance(sessionid, reportname, reportconditions, columnlist, pageno, numrows, numrecsinrpt, datefrom, dateto, pagedatefrom, pagedateto, pkid, dayinterval, sort)
                 values (t_sessionid, t_reportname, t_reportconditions, t_collist, t_pageno, t_numrows, t_numrecsinrpt, t_dtfrom, t_dtto, t_pagerunfrom, t_pagerunto, t_newpkid, t_dayinterval, t_sort);
-            select nvl(max(reportinstanceid), 0) into t_reportinstanceid from acct15m.report_instance;    
+            select nvl(max(reportinstanceid), 0) into t_reportinstanceid from report_instance;    
         else --update the existing row        
-            update acct15m.report_instance set pageno = t_pageno, pagedatefrom = t_pagerunfrom, pagedateto = t_pagerunto, pkid = t_newpkid where reportinstanceid = t_reportinstanceid;
+            update report_instance set pageno = t_pageno, pagedatefrom = t_pagerunfrom, pagedateto = t_pagerunto, pkid = t_newpkid where reportinstanceid = t_reportinstanceid;
         end if;
-        insert into acct15m.report_scroll(sessionid, reportinstanceid, reportname, pageno, numrows, datefrom, dateto, pagedatefrom, pagedateto, pkid, dayinterval)
+        insert into report_scroll(sessionid, reportinstanceid, reportname, pageno, numrows, datefrom, dateto, pagedatefrom, pagedateto, pkid, dayinterval)
             values (t_sessionid, t_reportinstanceid, t_reportname, t_pageno, t_numrows, t_dtfrom, t_dtto, t_pagerunfrom, t_pagerunto, t_newpkid, t_dayinterval);
 
     else  -- they want a specific page that is already generated
-        select pagedatefrom, pagedateto into t_newrunfrom, t_newrunto from acct15m.report_scroll where pageno = t_pageno_in and upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
+        select pagedatefrom, pagedateto into t_newrunfrom, t_newrunto from report_scroll where pageno = t_pageno_in and upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
         --need the PKID from the previous page
-        select nvl(max(pkid),0) into t_pkid from acct15m.report_scroll where pageno < t_pageno_in and upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
+        select nvl(max(pkid),0) into t_pkid from report_scroll where pageno < t_pageno_in and upper(sessionid) = upper(t_sessionid) and upper(reportname) = upper(t_reportname) and datefrom = t_dtfrom and dateto = t_dtto and createdon > sysdate - (1/24);
     end if;
     --set indicator to inform if more records in report
     if (t_pageno * t_numrows) >= t_numrecsinrpt then
@@ -256,13 +256,7 @@ END;
 
 /
 
-
-create view acct15m.acct_trans_vw1 as            
-SELECT A.ACC_ID as ACC_ID, A.ACC_NUMBER AS ACC_NUMBER, A.ACC_NAME AS ACC_NAME,T.BANK_ID AS BANK_ID, NULL AS RES_ID, NULL AS RES_CURRENCY_CODE, T.TX_CODE AS TX_CODE, NULL AS RES_AMOUNT, NULL AS RES_DATE, NULL AS RES_EXPIRE_DATE, T.TEXT AS TEXT, NULL AS MATCH_REF, NULL AS STATUS, NULL AS ORIGINAL_AMOUNT, NULL AS RES_REG_TIMESTAMP, NULL AS RES_CODE, T.TX_ID AS TX_ID, T.CURRENCY_CODE AS CURRENCY_CODE, T.AMOUNT AS AMOUNT, T.BOOK_DATE AS BOOK_DATE, T.DEBCRED_IND AS DEBCRED_IND, T.ORG_CURRENCY_CODE ORG_CURRENCY_CODE, T.EXCH_RATE AS EXCH_RATE, T.ORG_CUR_AMOUNT AS ORG_CUR_AMOUNT, T."DOMAIN" AS "DOMAIN", T.FAMILY AS FAMILY, T.SUB_FAMILY AS SUB_FAMILY, T.ETOE_REF AS ETOE_REF, T.PAYM_REF AS PAYM_REF, T.EXTERNAL_REF AS EXTERNAL_REF, T.ADD_REF AS ADD_REF, T.ADDITIONAL_ENTRY_INFO AS ADDITIONAL_ENTRY_INFO, T.INITIATOR AS INITIATOR, T.TX_CODE_TEXT AS TX_CODE_TEXT, T.VALUE_DATE AS VALUE_DATE, T.TRAN_PAYM_AMT_TEXT AS TRAN_PAYM_AMT_TEXT, T.REG_TIMESTAMP AS REG_TIMESTAMP, T.RESERVATION_ID AS RESERVATION_ID, T.EXCEP_CONTRA_TRANS AS EXCEP_CONTRA_TRANS, T.SENT_FROM_EXCEPTION AS SENT_FROM_EXCEPTION, T.ERROR_CODE AS ERROR_CODE, T.ERROR_ACCOUNT_REF AS ERROR_ACCOUNT_REF, T.MOVED_TO_ACCOUNT AS MOVED_TO_ACCOUNT, T.MOVED_TO_DATE AS MOVED_TO_DATE, T.RMTINF_USTRD AS RMTINF_USTRD, T.ACCT_REF AS ACCT_REF_IN_TX, T.VRA_NUM AS VRA_NUMBER, T.COND_FUNC_ID AS COND_FUNC_ID, T.COND_CODE AS COND_CODE
-			FROM acct15m.ACCOUNT A, acct15m.TRANSACTION T
-			WHERE
-			A.ACC_ID = T.ACC_ID;
-            
+--sample call
 
 DECLARE
   T_REPORTNAME VARCHAR2(200);
@@ -279,7 +273,7 @@ DECLARE
   T_HASMORE CHAR(200);
   T_REC_OUTPUT SYS_REFCURSOR;
 BEGIN
-  T_REPORTNAME := 'acct15m.acct_trans_vw1';
+  T_REPORTNAME := 'acct_trans_vw1';
   T_COLLIST := 'ACC_ID, ACC_NUMBER, ACC_NAME, BANK_ID, TX_CODE, BOOK_DATE, TX_ID';
   T_REPORTCONDITIONS := 'ACC_ID =1934863';
   T_PKCOLNAME := 'TX_ID';
